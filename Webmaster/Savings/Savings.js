0e56ee822704526c08aa2c756291efbf36e16971
aioverdrive <141070467+aioverdrive@users.noreply.github.com>
2024-02-19 06:13:26 -0800

    final touches
    1. made savings calculator more robust
    2. added more info for energies section
    3. gave proper format to info sources in documents
    4. added subtle text shadows to  headers
    i wanted to add some more info to the homepage but i think its getting kinda late lmao

--- Webmaster/Savings/Savings.js
+++ Webmaster/Savings/Savings.js
@@ -1,12 +1,43 @@
 function calculateTaxRebate() {
   const cost = parseFloat(document.getElementById('cost').value);
+  let year = parseFloat(document.getElementById('years').value);
 
   // Calculating the tax credit as 30% of the cost of green energy solutions
   const taxCredit = cost * 0.3;
-
+  // Storing the number in a seperate variable
+  let finalTaxCredit = taxCredit.toFixed(2);
+  let totalTaxCredit = 0;
 
 
   const resultElement = document.getElementById('result');
-  resultElement.innerHTML = `Your potential tax credit is: $${taxCredit.toFixed(2)}`;
+  //If statement to check if tax credit exceeds 10k
+  if (finalTaxCredit > 10000.00 && year > 10){
+    finalTaxCredit = 10000.00;
+    year = 10;
+    totalTaxCredit = finalTaxCredit*year;
+    totalTaxCredit = totalTaxCredit.toFixed(2);
+    resultElement.innerHTML = `Your potential tax credit annually is: $${finalTaxCredit}. You've reached the maximum annual amount!
+    Your total credit amassed over ${year} years is <em>$${totalTaxCredit}!</em> (You cannot earn credit for more than 10 years.)`;
+  }
+  else if (finalTaxCredit > 10000.00){
+    finalTaxCredit = 10000.00;
+    totalTaxCredit = finalTaxCredit*year;
+    totalTaxCredit = totalTaxCredit.toFixed(2);
+    resultElement.innerHTML = `Your potential tax credit annually is: $${finalTaxCredit}. You've reached the maximum annual amount!
+    Your total credit amassed over ${year} year(s) is <em>$${totalTaxCredit}!</em>`;
+  }
+  else if (year > 10){
+    year = 10;
+    totalTaxCredit = finalTaxCredit*year;
+    totalTaxCredit = totalTaxCredit.toFixed(2);
+    resultElement.innerHTML = `Your potential tax credit annually is: $${finalTaxCredit}.
+    Your total credit amassed over ${year} years is <em>$${totalTaxCredit}!</em> (You cannot earn credit for more than 10 years.)`;
+  }
+  else {
+    totalTaxCredit = finalTaxCredit*year;
+    totalTaxCredit = totalTaxCredit.toFixed(2);
+    resultElement.innerHTML = `Your potential tax credit anually is: $${finalTaxCredit}.
+    Your total credit amassed over ${year} year(s) is <em>$${totalTaxCredit}!</em>`;
+  }
 }
 
